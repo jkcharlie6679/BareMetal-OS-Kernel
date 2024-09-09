9a3af25324318c8aca537e23a0767ef9697ed221
Tsung Yu Chan <jkcharlie6679@gmail.com>
2024-09-10 04:29:52 +0800

    chore / review bootloader of lab2 and cleanup

--- lab2/bootloader/inc/uart.h
+++ lab2/bootloader/inc/uart.h
@@ -1,6 +1,19 @@
+#ifndef UART_H
+#define UART_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void uart_init();
 void uart_send(unsigned int c);
 char uart_getc();
 void uart_puts(char *s);
 void uart_hex(unsigned int d);
 char uart_getc_pure();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- lab2/bootloader/main.c
+++ lab2/bootloader/main.c
@@ -1,48 +1,48 @@
+#include <stdint.h>
 #include "uart.h"
 #include "printf.h"
 
-extern char* _code_relocate_place;
-extern unsigned long long  __code_size;
-extern unsigned long long _start;
+extern char *_code_relocate_place;
+extern uint64_t __code_size;
+extern uint64_t _start;
 
-void code_relocate(char * addr, char * dtb);
+void code_relocate(char * addr, char * arg){
+  uint64_t size = (uint64_t)&__code_size;
+  char* start = (char *)&_start;
 
-int relocate=1;
+  for (uint64_t i = 0; i < size; i++)
+    addr[i] = start[i];
 
-void main(char* arg){
-  // register char* x0 asm("x0");
-  
-  char *_dtb = arg;
+  void (*relocation)(char *)  = (void (*) (char *))addr; 
+  relocation(arg);
+}
+
+int relocate = 1;
+
+void main (char* arg) {
   uart_init();
   char* reloc_place = (char*)&_code_relocate_place;
 
-  if(relocate){
+  if (relocate) {
     relocate = 0;
-    code_relocate(reloc_place, _dtb);
+    code_relocate(reloc_place, arg);
   }
 
-  unsigned long long kernel_size = 0;
+  uint64_t kernel_size = 0;
   char* kernel_start = (char*) 0x80000;
   char c;
-  printf("Welcome UART bootloader!!\n\rPlease input the kernel size:\n\r");
-  for(int i = 0; i < 4; i++){
-    c = uart_getc_pure();
-    kernel_size += c<<(i*8);
+  
+  printf("Welcome UART bootloader!!\n\r");
+
+  for (int i = 0; i < 4; i++) {
+    /*c = uart_getc_pure();*/
+    kernel_size += uart_getc_pure() << (i * 8);
   }
-  for(int i = 0; i < kernel_size; i++){
-    c = uart_getc_pure();
-    kernel_start[i] = c;
+  for (int i = 0; i < kernel_size; i++) {
+    /*c = uart_getc_pure();*/
+    kernel_start[i] = uart_getc_pure();
   }
+
   void (*kernel) (char *) = (void (*) (char *))kernel_start;
   kernel(arg);
-}
-
-void code_relocate(char * addr, char * dtb){
-  unsigned long long size = (unsigned long long)&__code_size;
-  char* start = (char *)&_start;
-  for(unsigned long long i=0; i<size; i++){
-    addr[i] = start[i];
-  }
-  void (*relocation)(char *)  = (void (*) (char *))addr; 
-  relocation(dtb);
 }
