9aab5dd094dfe9480f3575847b02096d6167f956
Tsung Yu Chan <jkcharlie6679@gmail.com>
2024-09-10 18:34:22 +0800

    feat / basic 1 EL2 to EL1

---
+++ inc/cpio.h
@@ -0,0 +1,34 @@
+#ifndef CPIO_H
+#define CPIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct cpio_header {
+  char c_magic[6];
+  char c_ino[8];
+  char c_mode[8];
+  char c_uid[8];
+  char c_gid[8];
+  char c_nlink[8];
+  char c_mtime[8];
+  char c_filesize[8];
+  char c_devmajor[8];
+  char c_devminor[8];
+  char c_rdevmajor[8];
+  char c_rdevminor[8];
+  char c_namesize[8];
+  char c_check[8];
+} cpio_header;
+
+extern void* CPIO_DEFAULT_PLACE;
+
+void cpio_ls();
+void cpio_cat(char *str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ inc/mailbox.h
@@ -0,0 +1,33 @@
+#ifndef MAILBOX_H
+#define MAILBOX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+/* channels */
+#define MBOX_CH_POWER   0
+#define MBOX_CH_FB      1
+#define MBOX_CH_VUART   2
+#define MBOX_CH_VCHIQ   3
+#define MBOX_CH_LEDS    4
+#define MBOX_CH_BTNS    5
+#define MBOX_CH_TOUCH   6
+#define MBOX_CH_COUNT   7
+#define MBOX_CH_PROP    8
+/* tags */
+#define MAILBOX_TAG_BOARD_VISION     0x10002
+#define MAILBOX_TAG_MEMORY           0x10005
+
+int mailbox_property(unsigned int identifier, unsigned int buffer_size, unsigned char ch);
+
+void mbox_board_ver(uint32_t *board_ver);
+void mbox_mem_info(uint32_t *mem_start_addr, uint32_t *mem_size);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
